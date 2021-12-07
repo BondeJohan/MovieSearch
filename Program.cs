57634060250e07f5ba209232d63704b491f1495d
BondeJohan <johan.andersson3086@outlook.com>
2021-12-07 16:03:56 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Johan Andersson 2021-12-07
+
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MovieSearch
+{
+    class Program
+    {
+        public static HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            string uri = @"";
+
+            var respone = await client.GetAsync(uri);
+            respone.EnsureSuccessStatusCode();
+
+            string responeContent = await respone.Content.ReadAsStringAsync();
+
+            FindMoiveByID test = JsonConvert.DeserializeObject<FindMoiveByID>(responeContent);
+
+            test.DisplayFoundMoive();
+        }
+    }
+}
