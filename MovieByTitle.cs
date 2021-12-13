e8753ee6a89cb69a206e405441c266f48f8c745f
BondeJohan <johan.andersson3086@outlook.com>
2021-12-13 11:52:05 +0100

    Added a class and a try catch

---
+++ MovieByTitle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieSearch
+{
+    class MovieByTitle
+    {
+        public static List<MovieByTitle> MovieList = new List<MovieByTitle>();
+        public Result[] Results { get; set; }
+        public int Total_Results { get; set; }
+
+
+        public class Result
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+        }
+        public void SearchMovieByTitle()
+        {
+
+        }
+        public void DisplayListOfMovies()
+        {
+            foreach (var movieFound in MovieList)
+            {
+                Console.WriteLine(movieFound.Total_Results);
+                foreach (var item in MovieList[0].Results)
+                {
+                    Console.WriteLine("{0} {1}", item.Id, item.Title);
+                }
+            }
+        }
+    }
+}
